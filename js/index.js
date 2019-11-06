995b7727e730b771eeee2068989a66dfd6189ad9
HcwXd <e04dl47878@gmail.com>
2019-11-06 19:27:01 +0800

    Add intro and desc

--- js/index.js
+++ js/index.js
@@ -1,48 +1,31 @@
+document.querySelector('.desc').innerHTML = `
+A martingale is any of a class of betting strategies that originated from and were popular in 18th century France. 
+
+The simplest of these strategies was designed for a game in which the gambler wins the stake if a coin comes up heads and loses it if the coin comes up tails. The strategy had the gambler double the bet after every loss, so that the first win would recover all previous losses plus win a profit equal to the original stake. 
+
+You can look up more on <a href="https://en.wikipedia.org/wiki/Martingale_(betting_system)">wiki</a>.
+`;
+
+document.querySelector('.start_btn').addEventListener('click', () => {
+  document.querySelector('.intro').style.display = 'none';
+});
+
 const endowment = 100;
-var minBet = 0.01;
+let minBet = 0.01;
 const numberOfPlayer = 99;
-var roundSpeed = 10;
-var speed = 100;
+let roundSpeed = 10;
+let speed = 100;
 
-var container = document.querySelector(".container");
-var dashboard = document.querySelector(".dashboard");
-var start = document.querySelector(".start");
-var input_container = document.querySelector(".input-container");
-var betInput = document.querySelector(".bet-input")
-var speedInput = document.querySelector(".speed-input")
-
-
+let container = document.querySelector('.container');
+let dashboard = document.querySelector('.dashboard');
+let start = document.querySelector('.start');
+let input_container = document.querySelector('.input-container');
+let betInput = document.querySelector('.bet-input');
+let speedInput = document.querySelector('.speed-input');
 
 function playOneGame() {
-  return (Math.floor((Math.random() * 2)) !== 0) ? false : true;
+  return Math.floor(Math.random() * 2) !== 0 ? false : true;
 }
-
-// function startGames(number, initialMoney) {
-//   var bet = 1;
-//   var money = initialMoney;
-//   for (let i = 0; i < number; i++) {
-//     var win = playOneGame();
-//     if (win) {
-//       money += bet;
-//       bet = 1;
-//     } else {
-//       money -= bet;
-//       bet = bet * 2;
-//     }
-//   }
-//   return money;
-// }
-
-// function showResult() {
-//   renderInitial();
-//   for (let i = 0; i < 100; i++) {
-//     var moneyBar = document.createElement("DIV");
-//     moneyBar.className = "money";
-//     moneyBar.style.height = startGames(100, 100) + "px";
-//     console.log(moneyBar.style.height);
-//     container.appendChild(moneyBar);
-//   }
-// }
 
 function showProcess() {
   minBet = betInput.value ? parseInt(betInput.value) : 0.01;
@@ -51,23 +34,22 @@ function showProcess() {
   speed = speedInput.value ? parseInt(speedInput.value) : 100;
   roundSpeed = 1000 / speed;
 
-  input_container.style.display = "none";
+  input_container.style.display = 'none';
   renderInitial();
-  var players = [];
+  let players = [];
   for (let i = 0; i < numberOfPlayer; i++) {
     const player = {
       bet: minBet,
-      money: endowment
+      money: endowment,
     };
     players.push(player);
-    var moneyBar = document.createElement("DIV");
-    moneyBar.className = "money";
-    moneyBar.style.height = player.money + "px";
+    let moneyBar = document.createElement('DIV');
+    moneyBar.className = 'money';
+    moneyBar.style.height = player.money + 'px';
     container.appendChild(moneyBar);
   }
 
-
-  var round = 0;
+  let round = 0;
   setInterval(() => {
     while (container.firstChild) {
       container.removeChild(container.firstChild);
@@ -75,12 +57,12 @@ function showProcess() {
     renderInitial();
     for (let i = 0; i < numberOfPlayer; i++) {
       if (players[i].money < 0) {
-        var moneyBar = document.createElement("DIV");
-        moneyBar.className = "initial";
-        moneyBar.style.height = players[i].money + "px";
+        let moneyBar = document.createElement('DIV');
+        moneyBar.className = 'initial';
+        moneyBar.style.height = players[i].money + 'px';
         container.appendChild(moneyBar);
       } else {
-        var win = playOneGame();
+        let win = playOneGame();
         if (win) {
           players[i].money += players[i].bet;
           players[i].bet = minBet;
@@ -89,31 +71,28 @@ function showProcess() {
           players[i].bet = players[i].bet * 2;
         }
 
-
-        var moneyBar = document.createElement("DIV");
-        moneyBar.className = "money";
-        moneyBar.style.height = players[i].money + "px";
+        let moneyBar = document.createElement('DIV');
+        moneyBar.className = 'money';
+        moneyBar.style.height = players[i].money + 'px';
         container.appendChild(moneyBar);
       }
     }
     round++;
-    var broked = players.filter(player => {
+    let broked = players.filter(player => {
       return player.money < 0;
-    })
+    });
     dashboard.innerHTML = `Endowment: $${endowment} Bet: $${minBet} Player: ${numberOfPlayer} <br> Round: ${round} Broke: ${broked.length}`;
-
-  }, roundSpeed)
+  }, roundSpeed);
 }
 
 function renderInitial() {
-  var initialBar = document.createElement("DIV");
-  initialBar.className = "initial";
-  initialBar.style.backgroundColor = "green";
-  initialBar.style.height = endowment + "px";
+  let initialBar = document.createElement('DIV');
+  initialBar.className = 'initial';
+  initialBar.style.backgroundColor = 'green';
+  initialBar.style.height = endowment + 'px';
   container.appendChild(initialBar);
 }
 
-
-start.addEventListener("click", showProcess);
+start.addEventListener('click', showProcess);
 
 // showProcess();
